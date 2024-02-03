887cbff7d624ddc39382e7b1f7d61c74b8592ba9
Alexandr <alex@webgoal.ru>
2024-02-03 16:28:32 +0300

    Update

--- 04/01/src/sqrshape.cpp
+++ 04/01/src/sqrshape.cpp
@@ -6,18 +6,16 @@ SqrShape::SqrShape(int _x[4], int _y[4])
     type = TypeShape::SQR;
 
     // заполн¤ем координаты фигуры
-    x[0] = _x[0];
-    y[0] = _y[0];
-    x[1] = _x[1];
-    y[1] = _y[1];
-    x[2] = _x[2];
-    y[2] = _y[2];
-    x[3] = _x[3];
-    y[3] = _y[3];
+    for (size_t i = 0; i < 3; i++)
+    {
+        x[i] = _x[i];
+        y[i] = _y[i];
+    }
 
-     // стороны фигуры
-	int a = abs(x[0] - x[1]);
-	int b = abs(y[0] - y[1]);
+
+    // стороны фигуры
+    int a = abs(x[0] - x[1]);
+    int b = abs(y[0] - y[1]);
 
     // считаем площадь фигуры
     square = a * b;
