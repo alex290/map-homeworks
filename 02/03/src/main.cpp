4e33f1bf233390d13def139024845085ab483088
Alexandr <alex@webgoal.ru>
2024-02-03 20:05:13 +0300

    Update

--- 02/03/src/main.cpp
+++ 02/03/src/main.cpp
@@ -2,53 +2,74 @@
 #include <chrono>
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-void setProgress(int position, int ms)
+class Data
 {
-    for (size_t i = 0; i < position; i++)
+public:
+    Data(const long &n = 0) { numb = n; };
+    Data(const Data &val)
     {
-        std::cout << "\n";
-    }
+        this->numb = val.numb;
+    };
+    std::mutex mut;
+    long numb{};
+};
 
-    float progress = 0.0;
-    while (progress <= 1.0)
-    {
-        int barWidth = 70;
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+void swap_lock(Data &a, Data &b)
+{
+    std::lock(a.mut, b.mut);
+    std::lock_guard(a.mut, std::adopt_lock);
+    Data c(std::move(a));
+    a.numb = b.numb;
+    std::lock_guard(b.mut, std::adopt_lock);
+    b.numb = c.numb;
+};
 
-        std::cout << "[";
-        int pos = barWidth * progress;
-        for (int i = 0; i < barWidth; ++i)
-        {
-            if (i <= pos)
-                std::cout << char(219);
-            else
-                std::cout << " ";
-        }
-        std::cout << "] " << int(progress * 100.0) << " %";
-        std::cout << "\r";
-        // for (size_t i = 0; i < position; i++)
-        // {
-        //     std::cout << "\r";
-        // }
-        // std::cout.flush();
+void swap_unique_lock(Data &a, Data &b)
+{
+    std::unique_lock(a.mut, std::defer_lock);
+    Data c(std::move(a));
+    a.numb = b.numb;
+    std::unique_lock(b.mut, std::defer_lock);
+    b.numb = c.numb;
+    std::lock(a.mut, b.mut);
+};
 
-        progress += 0.01; // for demonstration only
-    }
-}
+void swap_scoped_lock(Data &a, Data &b)
+{
+    std::scoped_lock(a.mut, b.mut);
+    Data c(std::move(a));
+    a.numb = b.numb;
+    b.numb = c.numb;
+};
 
 int main()
 {
+    Data a{5};
+    Data b{8};
 
-    std::cout << "#\t Progress bar \t\t\t time" << std::endl;
+    Data a1{5};
+    Data b1{8};
 
-    std::thread j1(setProgress, 0, 50);
-    std::thread j2(setProgress, 2, 30);
+    Data a2{5};
+    Data b2{8};
+    std::cout << "a: " << a.numb << "; b: " << b.numb << std::endl;
+    std::cout << "a: " << a1.numb << "; b: " << b1.numb << std::endl;
+    std::cout << "a: " << a2.numb << "; b: " << b2.numb << std::endl;
+    std::cout << "================" << std::endl;
 
-    j1.join();
-    j2.join();
 
-    std::cout << std::endl;
+    std::thread sw_l(swap_lock, std::ref(a), std::ref(b));
+    std::thread sw_ul(swap_unique_lock, std::ref(a1), std::ref(b1));
+    std::thread sw_sl(swap_scoped_lock, std::ref(a2), std::ref(b2));
 
+    sw_l.join();
+    sw_ul.join();
+    sw_sl.join();
+
+    std::cout << "a: " << a.numb << "; b: " << b.numb << std::endl;
+    std::cout << "a: " << a1.numb << "; b: " << b1.numb << std::endl;
+    std::cout << "a: " << a2.numb << "; b: " << b2.numb << std::endl;
     return 0;
 }
